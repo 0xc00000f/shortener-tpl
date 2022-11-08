42ba35dfc820b9d2168e704ef3bb1e8f6dee6d3f
Maxim Tsyganov <0xc00000f@gmail.com>
2022-11-08 23:55:33 +0600

    small change logs

--- internal/storage/memory.go
+++ internal/storage/memory.go
@@ -29,7 +29,6 @@ func NewMemoryStorage(logger *zap.Logger) MemoryStorage {
 
 func (ms MemoryStorage) Get(short string) (long string, err error) {
 	ms.l.Info("input", zap.String("short", short))
-	defer ms.l.Info("function result", zap.String("long", long), zap.Error(err))
 
 	if len(short) == 0 {
 		err = ErrEmptyKey
@@ -39,13 +38,13 @@ func (ms MemoryStorage) Get(short string) (long string, err error) {
 	if !ok {
 		return "", ErrNoKeyFound
 	}
+
+	ms.l.Info("function result", zap.String("long", long), zap.Error(err))
 	return long, nil
 }
 
 func (ms MemoryStorage) Store(userID uuid.UUID, short, long string) (err error) {
-	defer ms.l.Info("function result history map", log.MapToFields(ms.history[userID])...)
-	defer ms.l.Info("function result storage map", log.MapToFields(ms.storage)...)
-	defer ms.l.Info("input + function result error",
+	ms.l.Info("input",
 		zap.String("userID", userID.String()),
 		zap.String("short", short),
 		zap.String("long", long),
@@ -65,6 +64,9 @@ func (ms MemoryStorage) Store(userID uuid.UUID, short, long string) (err error) 
 		ms.history[userID][short] = long
 	}
 	ms.storage[short] = long
+
+	ms.l.Info("function result history map", log.MapToFields(ms.history[userID])...)
+	ms.l.Info("function result storage map", log.MapToFields(ms.storage)...)
 	return nil
 }
 
@@ -75,6 +77,7 @@ func (ms MemoryStorage) IsKeyExist(short string) (bool, error) {
 
 func (ms MemoryStorage) GetAll(uuid uuid.UUID) (result map[string]string, err error) {
 	ms.l.Info("function input", zap.String("uuid", uuid.String()))
-	defer ms.l.Info("function result", log.MapToFields(result)...)
-	return ms.history[uuid], nil
+	result = ms.history[uuid]
+	ms.l.Info("function result", log.MapToFields(result)...)
+	return result, nil
 }
