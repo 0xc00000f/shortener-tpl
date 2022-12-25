7367dbad9bc65953eb900cef4cad0ad2dec87920
Maksim Ciganov <m.ciganov@gaijin.team>
2022-12-25 13:16:56 +0600

    redirect status gone if deleted

--- internal/handlers/redirect.go
+++ internal/handlers/redirect.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/0xc00000f/shortener-tpl/internal/shortener"
+	"github.com/0xc00000f/shortener-tpl/internal/storage"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -14,7 +16,14 @@ func Redirect(sa *shortener.NaiveShortener) http.HandlerFunc {
 
 		long, err := sa.Encoder().Get(r.Context(), short)
 		if err != nil {
+			var urlDeletedError storage.URLDeletedError
+			if errors.As(err, &urlDeletedError) {
+				http.Error(w, err.Error(), http.StatusGone)
+				return
+			}
+
 			http.Error(w, "400 page not found", http.StatusBadRequest)
+
 			return
 		}
 
